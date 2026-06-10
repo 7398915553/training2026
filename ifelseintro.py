57aa2be91bdb44497bed4c01a904155cfeb7861a
Techsrijan <aswanibtech@gmail.com>
2026-06-10 11:45:28 +0530

    if else intro

---
+++ ifelseintro.py
@@ -0,0 +1,7 @@
+a=int(input("Enter any number"))
+b=int(input("enter any number"))
+print("A=",a,"B=",b)
+if a>b:
+    print("A is greater than B")
+else:
+   print("B is greater than A")
---
+++ iftest.py
@@ -0,0 +1,20 @@
+'''
+if -10:
+    print("hi")
+else:
+    print("bye")
+
+if not True:
+    print("hi")
+else:
+    print("bye")'''
+
+x=5
+if x==1:
+    print("test")
+
+if x==5:
+    print("best")
+
+if x=5:
+    print("sab sahi hi")
